261f10156f331a4bcb84b335e48df5db83227875
sakibtamim <sakib.tamim@gmail.com>
2023-07-16 21:26:11 +0600

    second commit

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/design')
+def design():
+    return render_template('design.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
