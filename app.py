1799d2a67f7ad2fd14145fe9d3156c83e9793335
sakibtamim <sakib.tamim@gmail.com>
2023-07-18 01:55:05 +0600

    updated

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ def upload():
 
         #Image uploading method
         img = request.files["pic"]
-        img.save(f"static/images/{img.filename}")
+        img.save(f"static/dp's/{img.filename}")
         img_new_name = f"{key}{img.filename}"
-        os.rename(f"static/images/{img.filename}",f"static/images/{img_new_name}")
+        os.rename(f"static/dp's/{img.filename}",f"static/dp's/{img_new_name}")
     return render_template('design1.html', fname = firstname, lname = lastname, dprofession = profession, dschool = school, img = img_new_name,
                            dcollege = college, duniversity = university, daddress = address, dphone = phone, dmail = email, dabout= about, dskill1 = skill1,
                            dskill2 = skill2, dskill3 = skill3, dskill4 = skill4, dgit = git, dfb=fb, dlinkedin=linkedin)
